f10b63d6fb8c8dd0231dcb87fa2a7214a0890976
Bálint Tóth <tbalint57@gmail.com>
2024-12-10 23:48:24 +0100

    project assess visulise

--- fynesse/utils/pandas_utils.py
+++ fynesse/utils/pandas_utils.py
@@ -19,13 +19,13 @@ def load_csv(file_name, columns=None, column_names=None, index=None):
     return df
 
 
-def normalise_data_frame(df, columns_to_leave_oyt):
+def normalise_data_frame(df, columns_to_leave_out=[]):
     """
     Normalise data frame
     """
     normalised_df = df.copy()
 
-    feature_columns = df.columns.difference(columns_to_leave_oyt)
+    feature_columns = df.columns.difference(columns_to_leave_out)
     numerical_df = df[feature_columns]
 
     normalised_features = (numerical_df - numerical_df.min()) / (numerical_df.max() - numerical_df.min())
