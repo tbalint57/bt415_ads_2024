90fd3108e4b6cdf7216097d6ddf00760c1a756a1
Bálint Tóth <tbalint57@gmail.com>
2024-12-11 11:54:29 +0100

    project assess visulise transport data

--- fynesse/utils/plot_utils.py
+++ fynesse/utils/plot_utils.py
@@ -12,16 +12,16 @@ def plot_arrays(arrays, labels=None, colours=None, title=None, xlabel=None, ylab
 
         plt.plot(x, array, label=label, color=color)
 
-    if labels:
+    if labels is not None:
         plt.legend()
 
-    if title:
+    if title is not None:
         plt.title(title)
 
-    if xlabel:
+    if xlabel is not None:
         plt.xlabel(xlabel)
 
-    if ylabel:
+    if ylabel is not None:
         plt.ylabel(ylabel)
 
     plt.grid(True)
@@ -42,8 +42,8 @@ def visualise_feature_outliers(features_df, num_outliers=500, feature_labels=Non
 
     sorted_feature_arrays = [np.sort(feature_array) for feature_array in np.transpose(features_df.to_numpy())]
 
-    sorted_feature_arrays_mins = [feature_array[:num_outliers] for feature_array in np.transpose(features_df.to_numpy())]
-    sorted_feature_arrays_maxs = [feature_array[-num_outliers:] for feature_array in np.transpose(features_df.to_numpy())]
+    sorted_feature_arrays_mins = [feature_array[:num_outliers] for feature_array in sorted_feature_arrays]
+    sorted_feature_arrays_maxs = [feature_array[-num_outliers:] for feature_array in sorted_feature_arrays]
 
     plt.subplot(1, 2, 1)
     plot_arrays(sorted_feature_arrays_mins, labels=feature_labels, title=title, xlabel="n-th lowest value", ylabel="value")
