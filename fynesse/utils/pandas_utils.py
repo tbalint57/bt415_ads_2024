b5ad68653ef04e5b3880b14c05cb2a6cac1d717b
Bálint Tóth <tbalint57@gmail.com>
2024-12-15 20:51:33 +0100

    syncing pandas_utils

--- fynesse/utils/pandas_utils.py
+++ fynesse/utils/pandas_utils.py
@@ -21,15 +21,27 @@ def load_csv(file_name, columns=None, column_names=None, index=None):
 
 def normalise_data_frame(df, columns_to_leave_out=[]):
     """
-    Normalise data frame
+    Normalise data frame by each value being assigned (value - min) / (max - min)
     """
     normalised_df = df.copy()
 
     feature_columns = df.columns.difference(columns_to_leave_out)
     numerical_df = df[feature_columns]
-
     normalised_features = (numerical_df - numerical_df.min()) / (numerical_df.max() - numerical_df.min())
     
     normalised_df[numerical_df.columns] = normalised_features
+
+    return normalised_df
+
+
+def normalise_data_frame_by_rows(df, columns_to_leave_out=[]):
+    """
+    Normalise data frame by normalising each row so they sum up to 1
+    """
+    normalised_df = df.copy()
+
+    feature_columns = df.columns.difference(columns_to_leave_out)
+    normalised_df = df[feature_columns].div(df[feature_columns].sum(axis=1), axis=0)
+    normalised_df[columns_to_leave_out] = df[columns_to_leave_out]
 
     return normalised_df
